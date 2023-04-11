321387f3805db935b40119d7160d9fae0b0985af
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-04-11 12:45:17 +0200

    Created and styled button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import PrimaryButton from "./Components/PrimaryButton";
 import "./Styles/App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Hej</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Hej</h1>
+      <PrimaryButton />
+    </div>
+  );
 }
 
 export default App;
