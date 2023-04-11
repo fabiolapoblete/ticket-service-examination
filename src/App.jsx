31201e917a84d45a88c295b46e0eabf0437cdac2
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2023-04-11 14:39:46 +0200

    OrderItem added!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import OrderItem from "./Components/OrderItem";
 import "./Styles/App.css";
 
 function App() {
     return (
         <div className="App">
             <h1>Hej</h1>
+            <OrderItem /> 
         </div>
     );
 }
