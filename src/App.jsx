a426bc82576174989c8b0be2a94b6342f8c60e33
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-04-11 13:04:59 +0200

    merge app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 import PageTitle from "./Components/PageTitle";
-=======
-import PrimaryButton from "./Components/PrimaryButton";
->>>>>>> primaryButton
 import "./Styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hej</h1>
-<<<<<<< HEAD
       <PageTitle />
-=======
-      <PrimaryButton />
->>>>>>> primaryButton
     </div>
   );
 }
