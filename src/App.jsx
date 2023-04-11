dd14255349fc78a0e4834960994728e6eadbf719
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-04-11 12:51:19 +0200

    Component for page title

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import PageTitle from "./Components/PageTitle";
 import "./Styles/App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Hej</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Hej</h1>
+      <PageTitle />
+    </div>
+  );
 }
 
 export default App;
