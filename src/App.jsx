7a82f0a25e9cf99c831700fc5dd184a9e7596ae1
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-04-11 12:34:56 +0200

    Created and styled eventprice component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import EventPrice from "./Components/EventPrice";
 
 function App() {
-    return <div className="App"></div>;
+  return (
+    <div className="App">
+      <EventPrice />
+    </div>
+  );
 }
 
 export default App;
