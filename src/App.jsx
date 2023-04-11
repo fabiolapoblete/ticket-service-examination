fbd59541efb965c88509e5659a3133ba5aa22a4d
beratukiqi <berat.u@live.se>
2023-04-11 12:54:16 +0200

    EventItem Component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import EventItem from "./Components/EventItem";
 import "./Styles/App.css";
 
 function App() {
     return (
         <div className="App">
-            <h1>Hej</h1>
+            <EventItem />
         </div>
     );
 }
