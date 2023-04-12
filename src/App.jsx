10971df73fdc39473c4df83e4991ee56e6b1e534
adamsjodin <adamsjodin@hotmail.com>
2023-04-12 09:09:53 +0200

    Updated components and singlepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,14 @@ import LandingPage from "./Pages/LandinPage";
 import TicketsPage from "./Pages/TicketsPage";
 import "./Styles/App.css";
 import EventInfo from "./Components/EventInfo";
+import SinglePage from "./Pages/SinglePage";
 // import "./Styles/App.css";
 
 function App() {
     return (
         <div className="App">
             <h1>Hej</h1>
-            <PageTitle />
-            <FooterNav />
-            <LandingPage />
-            <TicketsPage />
-            <EventInfo />
+            <SinglePage />
         </div>
     );
 }
