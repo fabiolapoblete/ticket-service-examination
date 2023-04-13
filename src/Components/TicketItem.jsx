9d24a8cb7b9de7f1a175967fcf2ae716965464db
beratukiqi <berat.u@live.se>
2023-04-13 07:53:53 +0200

    TicketItem component added

---
+++ src/Components/TicketItem.jsx
@@ -0,0 +1,46 @@
+import "../Styles/TicketsPage.css";
+
+function TicketItem({ event }) {
+    const { name, price, where, when } = event;
+
+    return (
+        <article className="ticket__container">
+            <section className="ticket__artist">
+                <p className="ticket__sub-title">What</p>
+                <h1>{name}</h1>
+            </section>
+            <section className="ticket__location">
+                <p className="ticket__sub-title">Where</p>
+                <h2>{where}</h2>
+                <p>Göteborgs universitet, Pedagogen, hus A</p>
+            </section>
+            <section className="ticket__date-time">
+                <section>
+                    <p className="ticket__sub-title">When</p>
+                    <h3>{when.date}</h3>
+                </section>
+                <section>
+                    <p className="ticket__sub-title">From</p>
+                    <h3>{when.from}</h3>
+                </section>
+                <section>
+                    <p className="ticket__sub-title">To</p>
+                    <h3>{when.to}</h3>
+                </section>
+            </section>
+            <section className="ticket__info">
+                <p className="ticket__sub-title">Info</p>
+                <p>Section C - seat 233, bring umbrella</p>
+            </section>
+            <section className="ticket__barcode">
+                <img
+                    src="src\assets\barcode.png"
+                    alt="Scannable barcode representing your ticket"
+                />
+                <p>#A2ED7</p>
+            </section>
+        </article>
+    );
+}
+
+export default TicketItem;
--- src/Pages/TicketsPage.jsx
+++ src/Pages/TicketsPage.jsx
@@ -1,44 +1,8 @@
+import TicketItem from "../Components/TicketItem";
 import "../Styles/TicketsPage.css";
 
 function TicketsPage() {
-    return (
-        <article className="ticket__container">
-            <section className="ticket__artist">
-                <p className="ticket__sub-title">WHAT</p>
-                <h1>Lasse-Stefanz</h1>
-            </section>
-            <section className="ticket__location">
-                <p className="ticket__sub-title">Where</p>
-                <h2>Kjell Härnqvistsalen</h2>
-                <p>Göteborgs universitet, Pedagogen, hus A</p>
-            </section>
-            <section className="ticket__date-time">
-                <section>
-                    <p className="ticket__sub-title">When</p>
-                    <h3>21 Mar</h3>
-                </section>
-                <section>
-                    <p className="ticket__sub-title">From</p>
-                    <h3>19.00</h3>
-                </section>
-                <section>
-                    <p className="ticket__sub-title">To</p>
-                    <h3>21.00</h3>
-                </section>
-            </section>
-            <section className="ticket__info">
-                <p className="ticket__sub-title">Info</p>
-                <p>Section C - seat 233, bring umbrella</p>
-            </section>
-            <section className="ticket__barcode">
-                <img
-                    src="src\assets\barcode.png"
-                    alt="Scannable barcode representing your ticket"
-                />
-                <p>#A2ED7</p>
-            </section>
-        </article>
-    );
+    return <TicketItem event={event} />;
 }
 
 export default TicketsPage;
